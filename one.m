e81b2320bcc7683cc023255230cb9a880fb6037c
RoyGao <gaopeng@helmsman.im>
2017-03-20 11:55:34 +0800

    一部分在暂缓区准备commit，一部分在修改区

--- one.m
+++ one.m
@@ -1,1 +1,20 @@
 //好牛逼呀。直接打开文件。。。1
+//直接使用终端处理
+/*
+HelmsmandeMini:haswing_ios RoyGao$ cd /Users/nihong/Desktop
+-bash: cd: /Users/nihong/Desktop: No such file or directory
+HelmsmandeMini:haswing_ios RoyGao$ pwd
+/Users/RoyGao/Documents/haswing_ios
+HelmsmandeMini:haswing_ios RoyGao$ cd /Users/RoyGao/Desktop
+HelmsmandeMini:Desktop RoyGao$ mkdir test
+HelmsmandeMini:Desktop RoyGao$ git clone https://github.com/nihong/Temp.git
+Cloning into 'Temp'...
+remote: Counting objects: 6, done.
+remote: Compressing objects: 100% (3/3), done.
+remote: Total 6 (delta 0), reused 3 (delta 0), pack-reused 0
+Unpacking objects: 100% (6/6), done.
+HelmsmandeMini:Desktop RoyGao$ rm Temp
+rm: Temp: is a directory
+HelmsmandeMini:Desktop RoyGao$ cd Temp
+HelmsmandeMini:Temp RoyGao$ ls
+*/
